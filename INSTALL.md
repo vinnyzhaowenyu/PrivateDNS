9fdb375cc1057f4613d95efea03a819d40777d6a
vinnyzhaowenyu <admin@zhaowenyu.com>
2018-07-07 16:14:35 +0800

    deleted RunENV

--- INSTALL.md
+++ INSTALL.md
@@ -1,8 +1,9 @@
 # 安装指南
 
-## BIND9 安装
+## CentOS7 系统安装
+#### BIND9 安装
 
-## Python3 安装
+#### Python3 安装
 
-## Django2 安装
+#### Django2 安装
 
--- RunENV/CentOS7/Python-3.6.5/bin/django-admin.py
+++
@@ -1,6 +0,0 @@
-#!/usr/local/Python-3.6.5/bin/python3.6
-# EASY-INSTALL-DEV-SCRIPT: 'Django==2.0.7','django-admin.py'
-__requires__ = 'Django==2.0.7'
-__import__('pkg_resources').require('Django==2.0.7')
-__file__ = '/usr/local/Django-2.0.7/Django-2.0.7/django/bin/django-admin.py'
-exec(compile(open(__file__).read(), __file__, 'exec'))
